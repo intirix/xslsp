a6311f7b86c53be33e2990918458d8bc7f65a325
intirix <intirix@users.noreply.github.com>
2014-08-12 20:06:18 -0400

    Removed action message

--- src/main/java/com/intirix/xslsp/html/PageData.java
+++ src/main/java/com/intirix/xslsp/html/PageData.java
@@ -14,21 +14,4 @@ public class PageData implements Serializable
 	private static final long serialVersionUID = 1L;
 
 
-	private String actionMessage = "";
-
-
-	public String getActionMessage()
-	{
-		return actionMessage;
-	}
-
-
-	public void setActionMessage( String actionMessage )
-	{
-		this.actionMessage = actionMessage;
-	}
-	
-	
-
-
 }
